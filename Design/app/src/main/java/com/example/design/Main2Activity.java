f3c09b14edf36d631ee77d61106732d6a1be5c65
lynessadsouza <lynessadsouza@gmail.com>
2020-09-26 19:45:24 +0530

    My Assignments

---
+++ Design/app/src/main/java/com/example/design/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class Main2Activity extends AppCompatActivity
+{
+    Spinner Disrict;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
--- Design/app/src/main/java/com/example/design/MainActivity.java
+++ Design/app/src/main/java/com/example/design/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.design;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next=(TextView)findViewById(R.id.nextbtn);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
